d278727fb2f654900ff835a4e0cb966707cc7185
Ubuntu <richard@rr-terra-01.h5v2fswgypwufang1ebcwxdkkg.zx.internal.cloudapp.net>
2020-09-02 15:10:44 +0000

    First commit

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+location = "uksouth"
+resource_group = "rr-project03-rg"
+prefix = "rrproject03"
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+    type = string
+    description = "The location"
+}
+
+variable "resource_group" {
+    type = string
+    description = "The resource group"
+}
+
+variable "prefix" {
+    type = string
+    description = "The prefix"
+}
+
