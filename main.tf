1f17284f7427fa7246dafa9e680f388b85c3a098
Richard <rredgrave@gmail.com>
2020-10-15 12:42:12 +0000

    test

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~> 2.20.0"
-      
+
     }
   }
 }
 
 # Configure the Azure provider
 provider "azurerm" {
-    version = "~> 2.20.0"
-    features {}
-    use_msi = true
-    subscription_id = "0455ac28-cb65-423f-9960-b43bc679aa3e"
-    tenant_id       = "8316d3fa-e275-48ab-b9f0-bd5e6935ee14"
+  version = "~> 2.20.0"
+  features {}
+  use_msi         = true
+  subscription_id = "0455ac28-cb65-423f-9960-b43bc679aa3e"
+  tenant_id       = "8316d3fa-e275-48ab-b9f0-bd5e6935ee14"
 }
 
 resource "azurerm_resource_group" "main" {
@@ -44,7 +44,7 @@ resource "azurerm_public_ip" "pip" {
   name                = "${var.prefix}-pip"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_network_interface" "main" {
@@ -82,7 +82,7 @@ resource "azurerm_network_security_group" "webserver" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     source_address_prefix      = "*"
-    destination_port_range     = "443"
+    destination_port_range     = "80"
     destination_address_prefix = azurerm_subnet.internal.address_prefix
   }
 }
@@ -112,7 +112,27 @@ resource "azurerm_lb_backend_address_pool" "example" {
   name                = "BackEndAddressPool"
 }
 
+resource "azurerm_lb_probe" "example" {
+  resource_group_name = azurerm_resource_group.main.name
+  loadbalancer_id     = azurerm_lb.example.id
+  name                = "web-running-probe"
+  port                = 80
+}
 
+resource "azurerm_lb_rule" "example" {
+  resource_group_name            = azurerm_resource_group.main.name
+  loadbalancer_id                = azurerm_lb.example.id
+  name                           = "LBRule"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "PublicIPAddress"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.example.id
+  probe_id = azurerm_lb_probe.example.id
+}
+#test
+
+/*
 resource "azurerm_lb_nat_rule" "example" {
   resource_group_name            = azurerm_resource_group.main.name
   loadbalancer_id                = azurerm_lb.example.id
@@ -122,6 +142,7 @@ resource "azurerm_lb_nat_rule" "example" {
   backend_port                   = 443
   frontend_ip_configuration_name = azurerm_lb.example.frontend_ip_configuration[0].name
 }
+*/
 
 # Virtual Machine stuff
 
@@ -133,13 +154,13 @@ resource "azurerm_network_interface_backend_address_pool_association" "example" 
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
-  count                           = local.instance_count
-  name                            = "${var.prefix}-vm${count.index}"
-  resource_group_name             = azurerm_resource_group.main.name
-  location                        = azurerm_resource_group.main.location
-  size                            = "Standard_B2s"
-  admin_username                  = "richard"
-  availability_set_id             = azurerm_availability_set.avset.id
+  count               = local.instance_count
+  name                = "${var.prefix}-vm${count.index}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  size                = "Standard_B2s"
+  admin_username      = "richard"
+  availability_set_id = azurerm_availability_set.avset.id
   network_interface_ids = [
     azurerm_network_interface.main[count.index].id,
   ]
